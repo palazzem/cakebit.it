1861c71fb5f7bcd6a5fe351d9d36f44c7090e4e7
Emanuele Palazzetti <info@emanuelepalazzetti.eu>
2012-07-18 16:26:07 +0200

    Solved issue with Quicksand first category

--- js/script.js
+++ js/script.js
@@ -1,54 +1,49 @@
 $(document).ready(function(){
 
 	// Blur images on mouse over
-	$(".portfolio a").hover(function(){ 
-		$(this).children("img").animate({ opacity: 0.75 }, "fast"); 
-	}, function(){ 
-		$(this).children("img").animate({ opacity: 1.0 }, "slow"); 
-	}); 
-	
+	$(".portfolio a").hover(function(){
+		$(this).children("img").animate({ opacity: 0.75 }, "fast");
+	}, function(){
+		$(this).children("img").animate({ opacity: 1.0 }, "slow");
+	});
+
 	// Initialize prettyPhoto plugin
 	$(".portfolio a[rel^='prettyPhoto']").prettyPhoto({
-		theme:'light_square', 
-		autoplay_slideshow: false, 
-		overlay_gallery: false, 
+		theme:'light_square',
+		autoplay_slideshow: false,
+		overlay_gallery: false,
 		show_title: false
 	});
 
 	// Clone portfolio items to get a second collection for Quicksand plugin
 	var $portfolioClone = $(".portfolio").clone();
-	
+
 	// Attempt to call Quicksand on every click event handler
 	$(".filter a").click(function(e){
-		
-		$(".filter li").removeClass("current");	
-		
+
+		$(".filter li").removeClass("current");
+
 		// Get the class attribute value of the clicked link
 		var $filterClass = $(this).parent().attr("class");
+        var $filteredPortfolio = $portfolioClone.find("li[data-type~=" + $filterClass + "]");
 
-		if ( $filterClass == "all" ) {
-			var $filteredPortfolio = $portfolioClone.find("li");
-		} else {
-			var $filteredPortfolio = $portfolioClone.find("li[data-type~=" + $filterClass + "]");
-		}
-		
 		// Call quicksand
-		$(".portfolio").quicksand( $filteredPortfolio, { 
-			duration: 800, 
-			easing: 'easeInOutQuad' 
+		$(".portfolio").quicksand( $filteredPortfolio, {
+			duration: 800,
+			easing: 'easeInOutQuad'
 		}, function(){
-			
+
 			// Blur newly cloned portfolio items on mouse over and apply prettyPhoto
-			$(".portfolio a").hover( function(){ 
-				$(this).children("img").animate({ opacity: 0.75 }, "fast"); 
-			}, function(){ 
-				$(this).children("img").animate({ opacity: 1.0 }, "slow"); 
-			}); 
-			
+			$(".portfolio a").hover( function(){
+				$(this).children("img").animate({ opacity: 0.75 }, "fast");
+			}, function(){
+				$(this).children("img").animate({ opacity: 1.0 }, "slow");
+			});
+
 			$(".portfolio a[rel^='prettyPhoto']").prettyPhoto({
-				theme:'light_square', 
-				autoplay_slideshow: false, 
-				overlay_gallery: false, 
+				theme:'light_square',
+				autoplay_slideshow: false,
+				overlay_gallery: false,
 				show_title: false
 			});
 		});
@@ -58,5 +53,7 @@ $(document).ready(function(){
 
 		// Prevent the browser jump to the link anchor
 		e.preventDefault();
-	})
+	});
+
+    $("li a.first").trigger('click');
 });
