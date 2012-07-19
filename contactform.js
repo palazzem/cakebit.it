3149530b63b32e63d646dc9fb5d9b753892cb520
Emanuele Palazzetti <info@emanuelepalazzetti.eu>
2012-07-19 19:56:35 +0200

    Website architecture completed

--- contactform.js
+++ contactform.js
@@ -1,8 +1,8 @@
 $(document).ready(function() {
-	
+
 	//if submit button is clicked
-	$('#submit').click(function () {		
-		
+	$('#submit').click(function () {
+
 		//Get the data from all the fields
 		var name = $('input[name=name]');
 		var email = $('input[name=email]');
@@ -15,57 +15,57 @@ $(document).ready(function() {
 			name.addClass('hightlight');
 			return false;
 		} else name.removeClass('hightlight');
-		
+
 		if (email.val()=='') {
 			email.addClass('hightlight');
 			return false;
 		} else email.removeClass('hightlight');
-		
+
 		if (comment.val()=='') {
 			comment.addClass('hightlight');
 			return false;
 		} else comment.removeClass('hightlight');
-		
+
 		//organize the data properly
-		var data = 'name=' + name.val() + '&email=' + email.val() + '&website=' + 
+		var data = 'name=' + name.val() + '&email=' + email.val() + '&website=' +
 		website.val() + '&comment='  + encodeURIComponent(comment.val());
-		
+
 		//disabled all the text fields
 		$('.text').attr('disabled','true');
-		
+
 		//show the loading sign
 		$('.loading').show();
-		
+
 		//start the ajax
 		$.ajax({
 			//this is the php file that processes the data and send mail
-			url: "process.php",	
-			
+			url: "process.php",
+
 			//GET method is used
 			type: "GET",
 
-			//pass the data			
-			data: data,		
-			
+			//pass the data
+			data: data,
+
 			//Do not cache the page
 			cache: false,
-			
+
 			//success
-			success: function (html) {				
+			success: function (html) {
 				//if process.php returned 1/true (send mail success)
-				if (html==1) {					
+				if (html==1) {
 					//hide the form
-					$('.form').fadeOut('slow');					
-					
+					$('.form').fadeOut('slow');
+
 					//show the success message
 					$('.done').fadeIn('slow');
-					
+
 				//if process.php returned 0/false (send mail failed)
-				} else alert('Sorry, unexpected error. Please try again later.');				
-			}		
+				} else alert('Mi dispiace ma si &egrave; verificato un errore. Riprova tra poco.');
+			}
 		});
-		
+
 		//cancel the submit button default behaviours
 		return false;
-	});	
-});	
+	});
+});
--- process.php
+++ process.php
@@ -1,6 +1,6 @@
 <?php
 
-//Retrieve form data. 
+//Retrieve form data.
 //GET - user submitted data using AJAX
 //POST - in case user does not support javascript, we'll use POST instead
 $name = ($_GET['name']) ? $_GET['name'] : $_POST['name'];
@@ -12,47 +12,47 @@ $comment = ($_GET['comment']) ?$_GET['comment'] : $_POST['comment'];
 if ($_POST) $post=1;
 
 //Simple server side validation for POST data, of course, you should validate the email
-if (!$name) $errors[count($errors)] = 'Please enter your name.';
-if (!$email) $errors[count($errors)] = 'Please enter your email.'; 
-if (!$comment) $errors[count($errors)] = 'Please enter your comment.'; 
+if (!$name) $errors[count($errors)] = 'Per favore, inserisci il tuo nome.';
+if (!$email) $errors[count($errors)] = 'Per favore, inserisci la tua email.';
+if (!$comment) $errors[count($errors)] = 'Per favore, inserisci un messaggio.';
 
 //if the errors array is empty, send the mail
 if (!$errors) {
 
 	//recipient
-	$to = 'Rob Bettis <mail@blabl.com>';	 // <--------- change your email here
+	$to = 'Cakebit <info@cakebit.it>';
 	//sender
 	$from = $name . ' <' . $email . '>';
-	
+
 	//subject and the html message
-	$subject = 'Message from ' . $name;	
+	$subject = 'Richiesta informazioni';
 	$message = '
-	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" 
+	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 	<html xmlns="http://www.w3.org/1999/xhtml">
 	<head></head>
 	<body>
 	<table>
-		<tr><td>Name</td><td>' . $name . '</td></tr>
+		<tr><td>Nome mittente</td><td>' . $name . '</td></tr>
 		<tr><td>Email</td><td>' . $email . '</td></tr>
-		<tr><td>Message</td><td>' . nl2br($comment) . '</td></tr>
+		<tr><td>Messaggio</td><td>' . nl2br($comment) . '</td></tr>
 	</table>
 	</body>
 	</html>';
 
 	//send the mail
 	$result = sendmail($to, $subject, $message, $from);
-	
+
 	//if POST was used, display the message straight away
 	if ($_POST) {
-		if ($result) echo 'Thank you! We have received your message.';
-		else echo 'Sorry, unexpected error. Please try again later';
-		
-	//else if GET was used, return the boolean value so that 
+		if ($result) echo 'Grazie! Il tuo messaggio &egrave; stato ricevuto!';
+		else echo 'Mi dispiace ma si &egrave; verificato un errore. Riprova tra poco.';
+
+	//else if GET was used, return the boolean value so that
 	//ajax script can react accordingly
 	//1 means success, 0 means failed
 	} else {
-		echo $result;	
+		echo $result;
 	}
 
 //if the errors array has values
@@ -68,10 +68,10 @@ if (!$errors) {
 function sendmail($to, $subject, $message, $from) {
 	$headers = "MIME-Version: 1.0" . "\r\n";
 	$headers .= "Content-type:text/html;charset=iso-8859-1" . "\r\n";
-	$headers .= 'From: ' . $from . "\r\n";
-	
+	$headers .= 'Da: ' . $from . "\r\n";
+
 	$result = mail($to,$subject,$message,$headers);
-	
+
 	if ($result) return 1;
 	else return 0;
 }
